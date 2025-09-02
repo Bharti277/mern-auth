8da8d8b267d9114a5127116d9a480bed76472b6b
Bharti277 <bhartideependra277@gmail.com>
2025-09-02 21:43:05 +0530

    password wip

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const ResetPassword = () => {
   const [email, setEmail] = useState("");
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <form>
